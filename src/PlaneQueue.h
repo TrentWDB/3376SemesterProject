758146c1e599e01a00fc5a1cbbe8590271a8898c
Trent Davies <trentwdb@gmail.com>
2015-04-03 15:20:07 -0500

    set up basic maybe-working header file

--- src/PlaneQueue.h
+++ src/PlaneQueue.h
@@ -1,8 +1,16 @@
-#ifndef PLANE_QUEUE_H
-#define PLANE_QUEUE_H
+#ifndef PLANEQUEUE_H
+#define PLANEQUEUE_H
+
+#include <list>
+
+using namespace std;
 
 class PlaneQueue {
-  
+  list<Plane> planeList;
+  public:
+    void queuePlane(Plane& plane);
+    void dequeuePlane();
+    int getQueueLength();
 }
 
 #endif
